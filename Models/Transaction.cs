dcc771b047a3802c9c677b89b56460e883a50339
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-06-17 20:59:14 +0200

    Transaction model change

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -8,8 +8,12 @@ namespace SzParag.Models
     public class Transaction
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public string MyProperty { get; set; }
+        public string Memo { get; set; }
+        public decimal? Income { get; set; }
+        public decimal? Outcome { get; set; }
+        public string Category1 { get; set; }
+        public string Category2 { get; set; }
+        public string Category3 { get; set; }
 
     }
 }
